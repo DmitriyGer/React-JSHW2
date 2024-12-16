3be494b3ebf329764569e7d9e5cd29b321a51955
Dmitriy <dim1a15@mail.ru>
2024-12-16 14:13:03 +0300

    готовая работа

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommentsList from './CommentsList';
+
+function App() {
+  return (
+    <div>
+      <h1>Приложение с комментариями</h1>
+      <CommentsList />
+    </div>
+  );
+}
+
+export default App;
